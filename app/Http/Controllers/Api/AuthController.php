922423ce3549069341c5d619426654bb57b04208
0satriaw <48984162+0satriaw@users.noreply.github.com>
2020-11-27 18:12:57 +0800

    testing 3x

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -23,10 +23,9 @@ class AuthController extends Controller
             return response(['message'=>$validate->errors()],400); //return error invalid
 
         $registrationData['password'] = bcrypt($request->password); //enkripsi password
-        //testing
+
         // $user = User::create($registrationData)->sendEmailVerificationNotification();//DENGAN EMAIL BELUMMM
         $user = User::create($registrationData);
-        //COBA commit di branch satria
         return response([
             'message'=>'Register Success',
             'user'=>$user,
