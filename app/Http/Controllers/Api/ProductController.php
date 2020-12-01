7bed004733b8d0ea5aef9af57a07761fa5f0937d
stevanililyanis <stevanililyanisaputri@gmail.com>
2020-12-01 20:45:41 +0700

    crud ongkir & product

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Validator;
+use App\Product;
+
+class ProductController extends Controller
+{
+    public function index(){
+        $product = Product::all();
+
+        if(count($product)>0){
+                return response([
+                'message' =>'Retrieve All Success',
+                'data' =>$product
+                ],200);
+            }
+
+        return response([
+            'message' => 'Empty',
+            'data' =>null
+            ],404);
+        
+        
+    }
+
+    public function show ($id){
+        $product=Product::find($id);
+
+        
+        if(!is_null($product)){
+            return response([
+                'message'  => 'Retrieve Product Success',
+                'data' => $product
+            ],200);
+
+        }
+
+        return response([
+            'message' => 'Product Not Found',
+            'data' => null
+        ],404);
+    }
+
+    public function store(Request $request){
+        $storeData = $request->all();
+        $validate = Validator::make($storeData,[
+            'nama_product' => 'required|max:60|unique:product',
+            'deskripsi_product' => 'required|max:255',
+            'harga_product' => 'required|numeric',
+            'stok_product' => 'required|numeric'
+        ]);
+
+        if($validate->fails())
+            return response(['message'=> $validate->errors()],400);
+
+        $product = Product::create($storeData);
+        return response([
+            'message' => 'Add Product Success',
+            'data' => $product,
+        ],200);
+   
+
+
+    }
+
+    public function destroy($id){
+        $product = Product::find($id);
+
+            if(is_null($product)){
+                return response([
+                    'message' => 'Product Not Found',
+                    'data'=>null
+                ],404);
+            }
+
+            if($product->delete()){
+                return response([
+                    'message' => 'Delete Product Success',
+                    'data' =>$product,
+                ],200);
+            }
+            return response([
+                'message' => 'Delete Product Failed',
+                'data' => null,
+            ],400);
+        
+    }
+
+    public function update(Request $request, $id){
+        $product = Product::find($id);
+        if(is_null($product)){
+            return response([
+                'message'=>'Product Not Found',
+                'data'=>null
+            ],404);
+        }
+
+        $updateData = $request->all();
+        $validate = Validator::make($updateData,[
+            'nama_product'=>['max:60',Rule::unique('product')->ignore($product)],
+            'deskripsi_product' => 'max:255',
+            'harga_product' =>'numeric',
+            'stok_product'=>'numeric'
+        ]);
+
+        if($validate->fails())
+            return response(['message'=> $validate->errors()],400);
+            
+
+            $product->nama_product =  $updateData['nama_product'];
+            $product->deskripsi_product = $updateData['deskripsi_product'];
+            $product->harga_product= $updateData['harga_product'];
+            $product->stok_product = $updateData['stok_product'];
+
+        if($product->save()){
+            return response([
+                'message' => 'Update Product Success',
+                'data'=> $product,
+            ],200);
+        }
+
+        return response([
+            'messsage'=>'Update Product Failed',
+            'data'=>null,
+        ],400);
+    }
+
+    public function uploadGambar(Request $request, $id){
+        $product= Product::find($id);
+        if(is_null($product)){
+            return response([
+                'message' => 'Product not found',
+                'data' => null
+            ],404);
+        }
+
+        if(!$request->hasFile('gambar_product')) {
+            return response([
+                'message' => 'Upload Photo Product Failed',
+                'data' => null,
+            ],400);
+        }
+        $file = $request->file('gambar_product');
+        
+        if(!$file->isValid()) {
+            return response([
+                'message'=> 'Upload Photo Product Failed',
+                'data'=> null,
+            ],400);
+        }
+
+        $image = public_path().'/images/';
+        $file -> move($image, $file->getClientOriginalName());
+        $image = '/images/';
+        $image = $image.$file->getClientOriginalName();
+        $updateData = $request->all();
+        Validator::make($updateData, [
+            'gambar_product' => $image
+        ]);
+        $product->gambar_product = $image;
+
+
+        if($product->save()){
+            return response([
+                'message' => 'Upload Photo Product Success',
+                'path' => $image,
+            ],200);
+        }
+        
+        return response([
+            'messsage'=>'Upload Photo Product Failed',
+            'data'=>null,
+        ],400);
+        
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,23 @@ Route::post('login','Api\AuthController@login');
 // Route::get('email/verify/{id}','VerificationController@verify')->name('verification.verify');
 // Route::get('email/resend','VerificationController@resend')->name('verification.resend');
 
+    
+
+
 Route::group(['middleware'=>'auth:api'],function(){
-    // CRUD USER
+    
+    Route::get('product','Api\ProductController@index');
+    Route::get('product/{id}','Api\ProductController@show');
+    Route::post('product','Api\ProductController@store');
+    Route::put('product/{id}','Api\ProductController@update');
+    Route::delete('product/{id}','Api\ProductController@destroy');
+    Route::post('product/gambar_product/{id}', 'Api\ProductController@uploadGambar');
+
+    Route::get('ongkir','Api\OngkirController@index');
+    Route::get('ongkir/{id}','Api\OngkirController@show');
+    Route::post('ongkir','Api\OngkirController@store');
+    Route::put('ongkir/{id}','Api\OngkirController@update');
+    Route::delete('ongkir/{id}','Api\OngkirController@destroy');
+
     Route::post('logout','Api\AuthController@logout');
 });
